c758680a5946c2264148a01387dfd091d083152f
Dario <darioeschuky@yahoo.es>
2021-07-24 16:42:14 +0200

    Calculadora con JavaScript

---
+++ calculadora/js/script.js
@@ -0,0 +1,7 @@
+var num1 = 10;
+var num2 = 6;
+
+console.log('Suma = ' + (num1 + num2)); //Suma
+console.log('Resta = ' + (num1 - num2)); //Resta
+console.log('Multiplicacion = ' + (num1 * num2)); //Multiplicacion
+console.log('Division = ' + (num1 / num2)); //Division
