6923b9a036bb030f01e407d99faef563d9b997c0
Dario <darioeschuky@yahoo.es>
2021-07-24 17:40:56 +0200

    Proyecto de calificaciones

--- calificaciones/js/main.js
+++ calificaciones/js/main.js
@@ -1,5 +1,7 @@
-var nota = 8.7;
+var nota = 8;
 
+//Condition IF
+/*
 if (nota < 5) {
     console.log ('Insuficiente');
 } else if (nota >= 5 && nota < 6) {
@@ -12,4 +14,41 @@ if (nota < 5) {
     console.log ('Sobresaliente');
 } else {
     console.log ('Nota ilegible')
+}*/
+
+//Switch
+switch (nota) {
+    case 1:
+        console.log('Insuficiente');
+        break;
+    case 2:
+            console.log('Insuficiente');
+            break;
+    case 3:
+            console.log('Insuficiente');
+            break;
+    case 4:
+            console.log('Insuficiente');
+            break;
+    case 5:
+            console.log('Suficiente');
+            break;
+    case 6:
+            console.log('Bien');
+            break;
+    case 7:
+            console.log('Notable');
+            break;
+    case 8:
+            console.log('Notable');
+            break;                                                           
+    case 9:
+            console.log('Sobresaliente');
+            break;
+    case 10:
+            console.log('Sobresaliente');
+            break; 
+    default:
+            console.log('Nota ilegible');
+        break;
 }
